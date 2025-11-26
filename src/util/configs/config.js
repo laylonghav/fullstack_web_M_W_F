ca27f0f17901dffadba742a252882336700f4199
laylonghav <laylonghav2023@gmail.com>
2025-11-26 15:55:09 +0700

    update configs base_url

--- src/util/configs/config.js
+++ src/util/configs/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  base_url: "https://laravel-api-m-w-f.onrender.com/",
+  base_url: "https://laravel-api-m-w-f.onrender.com/api/",
   image_url: "http://127.0.0.1:8000/storage/",
 };
